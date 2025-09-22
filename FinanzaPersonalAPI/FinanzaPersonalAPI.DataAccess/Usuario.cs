f6bc607a1fabf11a330b98d945a55f0b55009482
Wellman Aranda <wellmanaranda@rocketmail.com>
2025-09-22 15:23:15 -0600

    Se crea el método InsertarUsuario en el proyecto DataAccess

--- FinanzaPersonalAPI/FinanzaPersonalAPI.DataAccess/Usuario.cs
+++ FinanzaPersonalAPI/FinanzaPersonalAPI.DataAccess/Usuario.cs
@@ -1,11 +1,23 @@
 ﻿using FinanzaPersonalAPI.DataAccess.Interface;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinanzaPersonalAPI.DataAccess
 {
+
+
     public class Usuario : IUsuario
     {
+        private readonly ConnectionManagerDbContext _context;
+
+        public Usuario(ConnectionManagerDbContext context)
+        {
+            _context = context;
+        }
+
         public void InsertarUsuario(Model.Usuario usuario)
         {
+            _context.Database
+                .ExecuteSqlInterpolated($"EXEC sp_Usuario_Insertar {usuario.Nombre}, {usuario.Correo}");
             //iusuario.InsertarUsuario(usuario);
         }
     }
