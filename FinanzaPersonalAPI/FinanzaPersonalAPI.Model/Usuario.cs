34342dc79ba2add7f901e0144c24a198ad3e9856
Wellman Aranda <wellmanaranda@rocketmail.com>
2025-09-22 21:22:57 -0600

    Se crea controlador UsuarioController

---
+++ FinanzaPersonalAPI/FinanzaPersonalAPI.Model/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace FinanzaPersonalAPI.Model
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Correo { get; set; }
+    }
+}
---
+++ FinanzaPersonalAPI/FinanzaPersonalAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,52 @@
+﻿using FinanzaPersonalAPI.BusinessLogic.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace FinanzaPersonalAPI.Controllers
+{
+    [ApiController]
+    [Route("")]
+
+    public class UsuarioController : ControllerBase
+    {
+        IUsuario iusuario;
+
+        public UsuarioController(IUsuario _usuario)
+        {
+            this.iusuario = _usuario;
+        }
+
+        [HttpPost]
+        [Route("CrearUsuario")]
+
+        public dynamic Datos([FromBody] Model.Usuario usuario)
+        {
+            try
+            {
+                var datosSerializados = JsonSerializer.Serialize(usuario);
+
+                var datosModelo = JsonSerializer.Deserialize<Model.Usuario>(datosSerializados);
+
+                iusuario.InsertarUsuario(datosModelo);
+
+                return new
+                {
+                    success = true,
+                    estado = StatusCode(200)
+                };
+            }
+            catch (Exception ex)
+            {
+                return new
+                {
+                    success = false,
+                    estado = ex.Message.ToString()
+                };
+            }
+
+
+
+
+        }
+    }
+}
