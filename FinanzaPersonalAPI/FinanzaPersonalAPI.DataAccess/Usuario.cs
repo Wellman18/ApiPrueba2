d1e5f631af19c025ac6451638acc84b9f96c323e
Wellman Aranda <wellmanaranda@rocketmail.com>
2025-09-22 21:41:23 -0600

    Se crea la capa de DataAccess

---
+++ FinanzaPersonalAPI/FinanzaPersonalAPI.DataAccess/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using FinanzaPersonalAPI.DataAccess.Interface;
+
+namespace FinanzaPersonalAPI.DataAccess
+{
+    public class Usuario : IUsuario
+    {
+
+
+        public void InsertarUsuario(Model.Usuario usuario)
+        {
+            //iusuario.InsertarUsuario(usuario);
+        }
+    }
+}
