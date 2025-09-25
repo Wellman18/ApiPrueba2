771b9aea66e1e022443bdf215d02c3194933a828
Wellman Aranda <wellmanaranda@rocketmail.com>
2025-09-25 16:51:26 -0600

    Se hicieron cambios en el modelo de Usuario

--- FinanzaPersonalAPI/FinanzaPersonalAPI.Model/Usuario.cs
+++ FinanzaPersonalAPI/FinanzaPersonalAPI.Model/Usuario.cs
@@ -4,8 +4,8 @@
     {
         public int Id { get; set; }
 
-        public string Nombre { get; set; }
+        public string? Nombre { get; set; }
 
-        public string Correo { get; set; }
+        public string? Correo { get; set; }
     }
 }
